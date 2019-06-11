0ce78498187ad0160cffd3234c13d883b5f363a4
luiszamudio4 <rodriguez.of.luis@gmail.com>
2019-06-10 19:40:33 -0500

    added queryurl and api key

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var soccerTeams = ["Arsenal","Chelsea","Tottenham"];
 // function to display soccer gifs
 function displaysoccergifs(){
 
+var soccer = $(this).attr("data-team");
+var queryURL ="https://api.giphy.com/v1/gifs/search?q=" + 
+soccer-teams + "&api_key=wRjy6YL9zPsAUHvF5vd0Uf3F81I4CQyC"
 }
 
 
