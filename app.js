90a2472e5d3111438003ea95c9c50f2a9c09bdca
luiszamudio4 <rodriguez.of.luis@gmail.com>
2019-06-10 19:16:50 -0500

    created array and started function to display gifs

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-// first array of cars
+// first array of soccer teams
+var soccerTeams = ["Arsenal","Chelsea","Tottenham"];
+
+// function to display soccer gifs
+function displaysoccergifs(){
+
+}
 
 
